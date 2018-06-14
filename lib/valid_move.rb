64fff9f284e8dff0e50338954ddcf9f8773b0af6
Dominique Demetz <ddemetz123@icloud.com>
2018-06-14 22:02:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,8 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move
 
+
+
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
