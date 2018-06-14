d666609ec20ae56ca910f4bcbea0838e168e845d
Dominique Demetz <ddemetz123@icloud.com>
2018-06-14 22:01:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,8 +13,6 @@ end
 def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board, index) == false
     return true
-    
-  else
-    return false
      end
+     return false
 end
